9ef7c3382522f2f592878860ec79e66b166209d5
KlivCode <infobrb04@gmail.com>
2025-06-23 08:23:54 +0545

    Updating the lab 1

--- AI/simple_reflex_agent.py
+++ AI/simple_reflex_agent.py
@@ -1,5 +1,6 @@
 # Lab1
 import random
+import time
 #case 1
 # Traffic Light Control Agent
 class TrafficEnvironment:
@@ -15,6 +16,7 @@ class TrafficEnvironment:
     def update_Environment(self):
         self.traffic_density=random.choice(["low", "high"])
         self.timer+=1
+
     def execute_action(self, action):
         if action == "SwitchToGreen":
             self.light="Green"
@@ -52,6 +54,7 @@ class RoomEnvironment:
     def update_environment(self):
         self.presence=random.choice(["True", "False"])
         self.time=random.choice(["Day","Night"])
+
     def execute_action(self,action):
         if action=="TurnOn":
             self.light="ON"
@@ -62,6 +65,27 @@ class RoomEnvironment:
         else:
             print("Action: No changes")
 
+#simulation 
+
+def simulate_traffic_light_agent(step=5):
+    print("\n----- Traffic Light Agent Simulation -----")
+    env= TrafficEnvironment()
+    agent=TrafficLightAgent()
+   
+
+    for step in range(step):
+      print(f"\n [Step: {step+1}]")
+      env.update_Environment()
+      percepts=env.get_percept()
+      print(f"Percept: Traffic Density {percepts[0]}, Timer {percepts[1]}, Light {percepts[2]}")
+      action=agent.decide(*percepts)
+      env.execute_action(action)
+      time.sleep(1)
+    print("\n----- Traffic Light Agent Simulation Ended -----")
+
+def simulate_smart_light_agent(step=5):
+    print("\n----- Smart Light Agent Simulation -----")
+    
 
 
 
